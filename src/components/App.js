a18175cf20e972211b798b7e88e2bdafe249d977
unknown <prateekrai341036@gmail.com>
2022-11-27 17:17:23 +0530

    Tab changing is done & fav -unfav works fine

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import { data } from '../data';
 import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies, setShowFavourites } from '../actions';
+
 
 class App extends React.Component {
   componentDidMount(){
@@ -15,31 +17,55 @@ class App extends React.Component {
 
     //make api call
     //dispatch action
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('State', store.getState());
   }
+
+  isMovieFavourite = (movie) => { 
+    const { favourites } = this.props.store.getState();                                //{list: [] ,favourites: []}
+
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      return true;                                            //movie founded
+    }
+
+    return false;                                          //movie not founded in favourite array
+  }
+
+  //tab changes handles here
+  onChangeTab = (val) => {
+    this.props.store.dispatch(setShowFavourites(val))
+  }
+
+
   
   render(){
-    const movies = this.props.store.getState();
-    console.log('RENDER');
+    const { list , favourites , showFavourites } = this.props.store.getState();                         //{list: [] ,favourites: []}
+    console.log('RENDER', this.props.store.getState());
+
+    const displayMovies = showFavourites ? favourites : list ;
     return (
       <div className="App">
         <Navbar />
         <div className="main">
           <div className="tabs">
-            <div className="tab"> Movies </div>
-            <div className="tab"> Favourites </div>
+            <div className={`tab ${showFavourites ? '' : 'active-tabs'}`}  onClick={() => this.onChangeTab(false)} > Movies </div>
+            <div className={`tab ${showFavourites ? 'active-tabs' : ''}`}  onClick={() => this.onChangeTab(true)} > Favourites </div>
           </div>
   
           <div className="list">
-            {movies.map((movie , index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+            {displayMovies.map((movie , index) => (
+              <MovieCard
+                movie={movie} 
+                key={`movies-${index}`} 
+                dispatch={this.props.store.dispatch}
+                isFavourite = {this.isMovieFavourite(movie)} 
+                />
             ))}
           </div>
+          {displayMovies.length === 0 ? <div className='no-movies'>no movies to display ! </div> : null}
         </div>
       </div>
     );
